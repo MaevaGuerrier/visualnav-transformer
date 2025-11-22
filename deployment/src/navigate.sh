d144526e44fada1740e8da12ac3cd757195bd904
MaevaGuerrier <87130407+MaevaGuerrier@users.noreply.github.com>
2025-11-21 20:18:50 -0500

    added navigate_onnx

--- deployment/src/navigate.sh
+++ deployment/src/navigate.sh
@@ -35,7 +35,8 @@ tmux splitw -h -p 50 # split it into two halves
 # Run the navigate.py script with command line args in the second pane
 tmux select-pane -t 0
 # tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python3 navigate.py $@" Enter
+tmux send-keys "python3 navigate_nomad_onnx.py $@" Enter
+# tmux send-keys "python3 navigate_normal.py $@" Enter
 
 # Run the pd_controller.py script in the fourth pane
 tmux select-pane -t 1
